c7a9d1bd724be48abfccc4ad347968538d66f110
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2022-07-24 16:40:48 -0300

    Finalizando cartas del dashboard

--- src/Components/CardsContainer.jsx
+++ src/Components/CardsContainer.jsx
@@ -3,8 +3,8 @@ import Card from './Card'
 import {useState, useEffect} from 'react'
 
 const cardsData = {
-    productos: {total:"Productos", ultimo: "Ultimo Producto"},
-    usuarios: {total: "Usuarios", ultimo:"Ultimo Usuario"},
+    productos: {total:"Productos", ultimo: "Ultimo Producto Creado"},
+    usuarios: {total: "Usuarios", ultimo:"Ultimo Usuario Creado"},
     ventas: {total: "Ventas", ultimo: "Ultima Venta"}
   }
 
@@ -12,18 +12,38 @@ const CardsContainer = () => {
 
   const [products, setProducts] = useState([])
   const [users, setUsers] = useState([])
+  const [sales, setSales] = useState([])
+
 
   useEffect (() => {
     //Fetch y Set de informacion de productos
-    const fetchData = (async () => {    
+    const fetchProductData = (async () => {    
       const res = await fetch ('http://localhost:3050/api/product')
       const data = await (res.json())
       return data
     })
-    fetchData().then ((data) => {
+    fetchProductData().then ((data) => {
       setProducts (data)
     })
 
+    //Fetch y Set de informacion de usuarios
+    const fetchUserData = (async () => {    
+      const res = await fetch ('http://localhost:3050/api/users')
+      const data = await (res.json())
+      return data
+    })
+    fetchUserData().then ((data) => {
+      setUsers (data)
+    })
+    //Fetch y Set de informacion de transacciones
+   const fetchSales = (async () => {    
+      const res = await fetch ('http://localhost:3050/api/sales')
+      const data = await (res.json())
+      return data
+    })
+    fetchSales().then ((data) => {
+      setSales (data)
+    })
   }, [])
 
 
@@ -39,6 +59,31 @@ const CardsContainer = () => {
     }
     {
       products.length === 0 &&
+      <h1>Cargando</h1>
+    }
+
+
+        {
+      users.length !== 0 &&
+      <> 
+      <Card  title={cardsData.usuarios.total} data={users.users.length}  />
+      <Card  title={cardsData.usuarios.ultimo} data={users.users[users.users.length-1].email}  />
+      </>
+    }
+    {
+      users.length === 0 &&
+      <h1>Cargando</h1>
+    }
+
+
+    {
+      sales.length !== 0 &&
+      <> 
+      <Card  title={cardsData.ventas.total} data={sales.data.length}  />
+      </>
+    }
+    {
+      users.length === 0 &&
       <h1>Cargando</h1>
     }
     </div>
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, Link } from "react-router-dom";
 import CardsContainer from './CardsContainer';
 import LastProduct from './LastProduct';
 import Categories from './Categories';
+import ProductList from './ProductList';
 
 const Sidebar = () => {
   return (
@@ -16,14 +17,16 @@ const Sidebar = () => {
         <Link to="/"><li className="navbar-item">Home</li></Link>
         <Link to="lastUser"><li className="navbar-item">Último usuario Creado</li></Link>
         <Link to="categories"><li className="navbar-item">Productos por Categoría</li></Link>
+        <Link to="productList"><li className="navbar-item">Lista de productos</li></Link>
       </ul>
     </nav>
   </div>
 
     <Routes>
+    <Route path="/" element={<CardsContainer />} />
       <Route path="lastUser" element={<LastProduct />} />
-      <Route path="/" element={<CardsContainer />} />
       <Route path="categories" element={<Categories />}/>
+      <Route path="productList" element={<ProductList />}/>
     </Routes>
     </>
   )
